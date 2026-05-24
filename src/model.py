6fa769e3ae78df28e273ba978d88342cda097edf
harouchisawsan-cyber <harouchisawsan@gmail.com>
2026-05-25 00:29:17 +0100

    Refactor DocumentClassifier initialization comments

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 #Contenu : La classe PyTorch (le CNN) et l'architecture de  modèle de classification.
+#Rôle : C'est le plan de construction de ton IA de classification.
 
 import torch
 import torch.nn as nn
@@ -7,13 +8,10 @@ from torchvision import models
 class DocumentClassifier(nn.Module):
     def __init__(self, num_classes=9):
         super(DocumentClassifier, self).__init__()
-        # On charge les poids pré-entraînés pour bénéficier de l'intelligence visuelle
         self.base_model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-        
-        # On remplace la tête du réseau pour tes 16 classes
         in_features = self.base_model.fc.in_features
         self.base_model.fc = nn.Sequential(
-            nn.Dropout(0.2), # Pour éviter l'overfitting
+            nn.Dropout(0.2),
             nn.Linear(in_features, num_classes)
         )
 
